22e3cb802512cd563d270bce50ab2c0c0680fca9
Talha Iqbal Syed <talhaiqbal11@gmail.com>
2018-09-16 04:21:48 +0500

    User data fetch from reducer

--- client/src/Components/User/profile/profile.js
+++ client/src/Components/User/profile/profile.js
@@ -4,7 +4,6 @@ import styles from './profile.css';
 import { connect } from 'react-redux';
 import { getUsers } from '../../../actions/userActions';
 import PropTypes from 'prop-types';
-import pp from "./dingus.jpg";
 
 class Profile extends Component {
   
@@ -16,39 +15,23 @@ class Profile extends Component {
     const { users } = this.props.user
     return (
       <div className="container-fluid cont">
-      { users.map(({ id, name }) => (
+      { users.map(({ id, name, picURL, about }) => (
         <div className="container">
         
           <div className="jumbotron text-left prof col-md-9">
-            <img src= { pp } className="pp col-md-2" alt="profile-picture"/>
+            <img src= { picURL } className="pp col-md-2" alt="profile-picture"/>
             <h2 className="col-md-7 name">Welcome Back {name}</h2>
             <p className="about">
-              Lorem ipsum dolor sit amet, 
-              nec sanctus debitis omittam id, 
-              vix soleat tibique id. Consetetur 
-              conclusionemque vel no, dicant 
-              nullam eam ei. Pri possim audiam 
-              inimicus id, ne veniam dicunt singulis 
-              per. Labore graecis nam ea, cum ei 
-              nonumy appareat, eos an unum nostrum. 
-              Propriae conclusionemque eum ea, porro 
-              nominati conceptam est ne, decore 
-              quaerendum voluptatibus ea nec.
-
-              Ei fugit congue tractatos vis. Id usu 
-              maiorum commune definitiones. Cu vis 
-              voluptua adipisci, alia ferri voluptatum 
-              nam an, nam te vide reque. Iriure nusquam 
-              ad pri.
+              { about }
             </p>
           </div>
           <div className="col-md-3 text-left">
           <button className="settingsButn col-md-12"><h4>Profile Settings</h4></button>
             <ul className="sidePanels">
-              <li><h4>Action</h4></li>
-              <li><h4>Action</h4></li>
-              <li><h4>Action</h4></li>
-              <li><h4>Action</h4></li>
+              <li>
+                <h3>Availability</h3>
+                <br/>More than 30 hrs/week
+              </li>
             </ul>
           </div>
         
